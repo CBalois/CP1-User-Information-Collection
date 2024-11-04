1b457b628d6f00d5af0273a9f28d0ecde08ebc06
Camelle <baloiscamelle@gmail.com>
2024-11-04 17:26:14 +0800

    git commit -m "initialcommit"

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let fullName = "Camelle Balois";
+let age = 20;
+let favoriteNumber = 25;
+let favoriteColor = "purple";
+
+console.log(fullName);
+console.log(age);
+console.log(favoriteNumber);
+console.log(favoriteColor);
